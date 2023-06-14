05d67af6aebc04d47d31ab56cbbbf07236228442
Ibexa Jenkins <jenkins@ibexa.co>
2023-06-14 12:55:58 +0000

    Template for v4.4.4

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const Encore = require('@symfony/webpack-encore');
+const path = require('path');
+const getIbexaConfig = require('./ibexa.webpack.config.js');
+const ibexaConfig = getIbexaConfig(Encore);
+const customConfigs = require('./ibexa.webpack.custom.configs.js');
+
+Encore.reset();
+Encore.setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .enableStimulusBridge('./assets/controllers.json')
+    .enableSassLoader()
+    .enableReactPreset()
+    .enableSingleRuntimeChunk()
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[ext]',
+        pattern: /\.(png|svg)$/,
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    });
+
+// Welcome page stylesheets
+Encore.addEntry('welcome-page-css', [
+    path.resolve(__dirname, './assets/scss/welcome-page.scss'),
+]);
+
+// Welcome page javascripts
+Encore.addEntry('welcome-page-js', [
+    path.resolve(__dirname, './assets/js/welcome.page.js'),
+]);
+
+Encore.addEntry('app', './assets/app.js');
+
+const projectConfig = Encore.getWebpackConfig();
+
+projectConfig.name = 'app';
+
+module.exports = [ibexaConfig, ...customConfigs, projectConfig];
+
+// uncomment this line if you've commented-out the above lines
+// module.exports = [ eZConfig, ibexaConfig, ...customConfigs ];
